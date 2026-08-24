c2ab4e97b52b3a80db0529f25356f141c1eefe08
Himanshu Kumar <himanshu.kumar@skyreon.com>
2026-08-24 15:42:42 +0530

    feat: Integrate Google Tag Manager into RootLayout for enhanced tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,31 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53DQXXLZ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53DQXXLZ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
+
+        {children}
+      </body>
     </html>
   );
 }
